3797c1a9d9039c84fdab02126f548baaaa083a0d
Josué Alves de Oliveira <josueao.mobile@gmail.com>
2021-01-16 05:16:24 -0300

    Find Category bt id, exception handling

---
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/StandardError.java
@@ -0,0 +1,60 @@
+package com.devsuperior.dscatalog.resources.exceptions;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+public class StandardError implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Instant timestemp;
+	private Integer status;
+	private String erros;
+	private String message;
+	private String path;
+	
+	public StandardError() {
+	}
+
+	public Instant getTimestemp() {
+		return timestemp;
+	}
+
+	public void setTimestemp(Instant timestemp) {
+		this.timestemp = timestemp;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getErros() {
+		return erros;
+	}
+
+	public void setErros(String erros) {
+		this.erros = erros;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+	
+
+}
