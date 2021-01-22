1b9a72f328e0a623b5966d23d619cd3351302ea0
Josué Alves de Oliveira <josueao.mobile@gmail.com>
2021-01-22 02:23:48 -0300

    Finish home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,13 +1,11 @@
 //import Reac, { useState, useEffect } from 'react';
-import './core/assets/styles/custom.scss';
-import './app.scss';
-import Navbar from './core/componentes/Navbar';
-
+import "./core/assets/styles/custom.scss";
+import "./app.scss";
+//import Navbar from "./core/componentes/Navbar";
+import Routes from "./Routes";
 
 const App = () => {
-    return (
-        <Navbar />
-    );
-}
+  return <Routes />;
+};
 
 export default App;
---
+++ front-web/src/pages/Catalog/components/ProductCard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ProductCard = () => (
+    <div className="card-base">
+        produto
+    </div>
+);
+
+export default ProductCard;
