9f283abae5b5257f3ff5726a4b28893e45a6fb9f
Josué Alves de Oliveira <josueao.mobile@gmail.com>
2021-01-29 04:59:14 -0300

    Finish catalog loaders

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -5,33 +5,43 @@ import ProductCard from "./components/ProductCard";
 import "./styles.scss";
 import { makeRequest } from "../../core/utilis/request";
 import { ProductsResponse } from "../../core/types/Products";
+import ProductCardLoader from "./components/Loaders/ProductCardLoader";
 
 const Catalog = () => {
   const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const params = {
       page: 0,
       linesPerPage: 12
     }
+
+    setIsLoading(true);
     makeRequest({ url: '/products', params})
-      .then(response => setProductsResponse(response.data));
+      .then(response => setProductsResponse(response.data))
+      .finally(() => {
+        setIsLoading(false);
+      })
 
   }, []);
 
   return (
     <div className="catalog-container">
-    <h1 className="catalog-title">
+     <h1 className="catalog-title">
         Catálogo de produtos
-    </h1>
-    <div className="catalog-products">
-      {productsResponse?.content.map(product => (
-        <Link to={`products/${product.id}`} key={product.id}>
-          <ProductCard product={product}/>
-        </Link> 
-      ))}       
-    </div>
-  </div>
+     </h1>
+     <div className="catalog-products">
+      {isLoading ? <ProductCardLoader /> :(
+        productsResponse?.content.map(product => (
+          <Link to={`products/${product.id}`} key={product.id}>
+            <ProductCard product={product}/>
+          </Link> 
+        ))
+      )}
+        
+     </div>
+   </div>
   );
 }
   
